dd9fdac2066379f56f3c4a912de71a76d6678e31
Peter Fillmore <peter@peterfillmore.com>
2015-03-31 16:35:29 +1100

    skeleton python stuff added

--- dumpfw.py
+++ dumpfw.py
@@ -1,17 +1,62 @@
-import serial
+from serial import * 
 #get handler
-ser = serial.Serial("/dev/tty.usbserial", 9600, timeout=1,parity=serial.PARITY_EVEN)
-
-try:
-    ser.write("\x7f") #enter bootloader
-    if(ser.read() != "y"):
-        raise exception serial.SerialException
-
-except exception serial.SerialException:
-    print "something happened?"
+ser = Serial("/dev/tty.usbserial", 9600, timeout=1,parity=PARITY_EVEN)
 
 def cmd_get(hser):
     hser.write("\x00\xff")
-    hser.read()
+    data = hser.read(15)
+    print data.encode("hex") 
+
+def cmd_verproc(hser):
+    hser.write("\x01\xfe")
+    data = hser.read(15)
+    print data.encode("hex") 
+        
+def cmd_getid(hser):
+    hser.write("\x02\xfd")
+    data = hser.read(15)
+    print data.encode("hex") 
+
+def cmd_readmem(hser,address):
+    hser.write("\x11\xee")
+    data = hser.read(1)
+    print data.encode("hex") 
+
+def cmd_writemem(hser):
+    hser.write("\x31\xce")
+    data = hser.read(1)
+    print data.encode("hex") 
+
+def cmd_erasemem(hser):
+    hser.write("\x43\xbc")
+    data = hser.read(1)
+    print data.encode("hex") 
+
+def cmd_extendederasemem(hser):
+    hser.write("\x44\xbb")
+    data = hser.read(1)
+    print data.encode("hex") 
+
+def cmd_writeunprotect(hser):
+    hser.write("\x73\x8c")
+    data = hser.read(1)
+    print data.encode("hex") 
+
+try:
+    ser.write("\x7f") #enter bootloader
+    resp = ser.read()
+    if(resp != "\x79"):
+        raise SerialException
+    cmd_get(ser) 
+    cmd_verproc(ser)  
+    #cmd_getid(ser)
+    #cmd_readmem(ser,0x0000)
+    #cmd_writemem(ser)
+    #cmd_extendederasemem(ser)
+    cmd_writeunprotect(ser)
  
+except SerialException:
+    print "something happened?"
+
+#def cmd_ 
 
